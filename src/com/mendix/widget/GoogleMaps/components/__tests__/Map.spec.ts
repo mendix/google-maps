dfd23190685ec4138fdf3fa943aa5cf8788306eb
Acellam Guy <abiccel@yahoo.com>
2016-10-26 13:12:52 +0300

    Added pending tests for map

--- src/com/mendix/widget/GoogleMaps/components/__tests__/Map.spec.ts
+++ src/com/mendix/widget/GoogleMaps/components/__tests__/Map.spec.ts
@@ -1,20 +1,52 @@
 import { shallow } from "enzyme";
 import { DOM, createElement } from "react";
 
-import { Map , MapProps } from "../Map";
+import { Map, MapProps } from "../Map";
 
 describe("Map component", () => {
     const testProps: MapProps = {
-         address: "Lumumba Ave, Kampala, Uganda",
-         apiKey: "AIzaSyACjBNesZXeRFx86N7RMCWiTQP5GT_jDec"};
+        address: "Lumumba Ave, Kampala, Uganda",
+        apiKey: "AIzaSyACjBNesZXeRFx86N7RMCWiTQP5GT_jDec"
+    };
 
-    const mapComponent = (props: MapProps) => shallow(createElement(Map, props));
+    const shallowMapComponent = (props: MapProps) => shallow(createElement(Map, props));
 
     it("should render google map structure", () => {
-        const output = mapComponent(testProps);
+        const output = shallowMapComponent(testProps);
 
         expect(output).toMatchStructure(
-           DOM.div({ className: "mx-google-maps" })
+            DOM.div({ className: "mx-google-maps" })
         );
+    });
+
+    xit("should handle empty address", () => {
+        // TODO: Implement test.
+    });
+
+    xit("should handle non existing address", () => {
+        // TODO: Implement test.
+    });
+
+    xit("should geocode address", () => {
+        // TODO: Implement test.
+    });
+
+    xit("should fail to geocode address", () => {
+        // TODO: Implement test.
+    });
+    xit("should center map to user address", () => {
+        // TODO: Implement test.
+    });
+
+    xit("should center map to default address", () => {
+        // TODO: Implement test.
+    });
+
+    xit("should check that google maps is defined ", () => {
+        // TODO: Implement test.
+    });
+
+    xit("should check that google maps is not defined", () => {
+        // TODO: Implement test.
     });
 });
