e1cb79ed2c48a4eafe3aacbd6e626bf761f5ed28
Andries Smit <mr.andries.smit@gmail.com>
2016-10-27 15:46:54 +0200

    Remove tsx settings webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,12 +11,12 @@ module.exports = {
         library: "com.mendix.widget.GoogleMaps.GoogleMaps"
     },
     resolve: {
-        extensions: [ "", ".ts", ".tsx", ".js", ".json" ]
+        extensions: [ "", ".ts", ".js", ".json" ]
     },
     errorDetails: true,
     module: {
         loaders: [
-            { test: /\.tsx?$/, loaders: [ "ts-loader" ] },
+            { test: /\.ts?$/, loaders: [ "ts-loader" ] },
             { test: /\.json$/, loader: "json" }
         ]
     },
