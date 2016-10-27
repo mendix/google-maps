a6d346e5d794b0c0219fd3cbaaa168e75a20ad69
Acellam Guy <abiccel@yahoo.com>
2016-10-27 17:53:01 +0300

    Test should fail geocode address done

--- src/com/mendix/widget/GoogleMaps/components/__tests__/Map.spec.ts
+++ src/com/mendix/widget/GoogleMaps/components/__tests__/Map.spec.ts
@@ -4,12 +4,13 @@ import { createElement } from "react";
 import { Map, MapProps } from "../Map";
 
 describe("Map component", () => {
-    let testProps: MapProps = {
+    const testProps: MapProps = {
         address: "Lumumba Ave, Kampala, Uganda",
         apiKey: "AIzaSyACjBNesZXeRFx86N7RMCWiTQP5GT_jDec"
     };
     let mountMapComponent = mount(createElement(Map, testProps));
     let map = mountMapComponent.instance() as Map;
+    const timeOut = 4000;
 
     it("should render google map structure", () => {
         expect(mountMapComponent.find("div.mx-google-maps").length).toBe(1);
@@ -26,7 +27,7 @@ describe("Map component", () => {
     it("should geocode address", (done) => {
         setTimeout(() => {
             map.getLocation(testProps.address, callback);
-        }, 4000);
+        }, timeOut);
         const callback = (coordinates: google.maps.LatLng) => {
             expect(coordinates.lat()).toBe(0.3229765);
             expect(coordinates.lng()).toBe(32.576219700000024);
@@ -36,8 +37,18 @@ describe("Map component", () => {
 
     });
 
-    xit("should fail to geocode address", () => {
-        // TODO: Implement test.
+    it("should fail to geocode address", (done) => {
+        setTimeout(() => {
+            const noAddress = "no address";
+            mountMapComponent.setProps({ address: noAddress });
+            map.getLocation(noAddress, callback);
+        }, timeOut);
+        const callback = (coordinates: google.maps.LatLng) => {
+            expect(coordinates.lat()).not.toBe(0.3229765);
+            expect(coordinates.lng()).not.toBe(32.576219700000024);
+
+            done();
+        };
     });
 
     xit("should center map to user address", () => {
