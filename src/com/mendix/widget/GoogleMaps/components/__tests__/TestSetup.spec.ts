36ebf8711bd62194c26bc409d11b65ad5b86857a
Akileng Isaac <isaacakileng56@gmail.com>
2016-10-31 11:42:15 +0300

    Tests outline

---
+++ src/com/mendix/widget/GoogleMaps/components/__tests__/TestSetup.spec.ts
@@ -0,0 +1,34 @@
+describe("Map", () => {
+
+    it("should render map structure", () => {
+        /** */
+    });
+
+    it("should handle an empty address", () => {
+        /** */
+    });
+
+    it("should geocode an address", () => {
+        /** */
+    });
+
+    it("should fail to geocode unknown address", () => {
+        /** */
+    });
+
+    it("should center the map to default center", () => {
+        /** */
+    });
+
+    it("should check that typeof google is defined", () => {
+        /** */
+    });
+
+    it("should check that typeof google is undefined", () => {
+        /** */
+    });
+
+    it("should create a marker using correct props", () => {
+        /** */
+    });
+});
