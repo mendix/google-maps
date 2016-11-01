4a7bd662999f6e6189fb483f7343f87a4c5e3715
Acellam Guy <abiccel@yahoo.com>
2016-11-01 12:44:53 +0300

    set up google mock in map spec

--- src/com/mendix/widget/GoogleMaps/components/__tests__/Map.spec.ts
+++ src/com/mendix/widget/GoogleMaps/components/__tests__/Map.spec.ts
@@ -1,4 +1,37 @@
+import { shallow } from "enzyme";
+import { createElement } from "react";
+
+import { Map, MapProps } from "../Map";
+
+import { EventMock, GeocoderLocationType, GeocoderMock, GeocoderStatus, LatLngBoundsMock,
+    LatLngMock, MapsMock, MarkerMock } from "../../../../../../../tests/mocks/GoogleMaps";
+
 describe("Map", () => {
+
+    window.google = window.google || {};
+    window.google.maps = window.google.maps || {};
+    window.google.event = window.google.event || {};
+    const originalMap = window.google.maps.Map;
+    const originalGeocoder = window.google.maps.Geocoder;
+    const originalLatLngBounds = window.google.maps.LatLngBounds;
+    const originalLatLng = window.google.maps.LatLng;
+    const originalMarker = window.google.maps.Marker;
+    const originalEvent = window.google.maps.event;
+
+    const address = "Lumumba Ave, Kampala, Uganda";
+
+
+    beforeEach(() => {
+        window.google.maps.Geocoder = GeocoderMock;
+        window.google.maps.Map = MapsMock;
+        window.google.maps.LatLngBounds = LatLngBoundsMock;
+        window.google.maps.LatLng = LatLngMock;
+        window.google.maps.Marker = MarkerMock;
+        window.google.maps.event = EventMock;
+        window.google.maps.GeocoderStatus = GeocoderStatus;
+        window.google.maps.GeocoderStatus = GeocoderLocationType;
+    });
+
     it("should render with the map structure", () => {
         //
     });
@@ -85,5 +118,16 @@ describe("Map", () => {
         it("should show a user error when looking up an address", () => {
             //
         });
+    });
+
+    afterEach(() => {
+        // Probably its good enough the reset fully.
+        // window.google = undefined;
+        window.google.maps.Map = originalMap;
+        window.google.maps.Geocoder = originalGeocoder;
+        window.google.maps.LatLngBounds = originalLatLngBounds;
+        window.google.maps.LatLng = originalLatLng;
+        window.google.maps.Marker = originalMarker;
+        window.google.maps.event = originalEvent;
     });
 });
