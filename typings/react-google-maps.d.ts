12353d91cb71beda427f1e1806db923a9b488c90
Akileng Isaac <isaacakileng56@gmail.com>
2016-11-11 17:57:07 +0300

    Refactor typings

--- typings/react-google-maps.d.ts
+++ typings/react-google-maps.d.ts
@@ -1,32 +1,35 @@
 declare module "react-google-maps" {
+    interface LatLng {
+        lat: number;
+        lng: number;
+    }
     interface GoogleMapProps {
-        defaultCenter: any;
+        center: LatLng;
         defaultZoom: number;
-        onClick:()=>{};
-        ref:()=>{};
+        ref:Function;
     }
     export const GoogleMap: React.ComponentClass<GoogleMapProps>;
     interface GoogleMapLoaderProps {
-        containerElement: any;
-        googleMapElement: any;
+        containerElement: React.ReactElement;
+        googleMapElement: React.ReactElement;
     }
     interface withGoogleMapProps {
-        containerElement: any;
-        mapElement: any;
+        containerElement: React.ReactElement;
+        mapElement: React.ReactElement;
     }
     interface MarkerProps {
         position: { lat: number; lng: number; };
     }
     export const GoogleMapLoader: React.ComponentClass<GoogleMapLoaderProps>;
     export const Marker: React.ComponentClass<MarkerProps>;
-    export function withGoogleMap(WrappedComponent: any): React.ComponentClass<withGoogleMapProps>;
+    export function withGoogleMap(WrappedComponent: React.Component): React.ComponentClass<withGoogleMapProps>;
 };
 declare module "react-google-maps/lib/async/withScriptjs" {
 
     interface withScriptjsProps {        
         googleMapURL: String;
-        loadingElement: any;
+        loadingElement: React.ReactElement;
     }
     
-    export default function withScriptjs(WrappedComponent: any): React.ComponentClass<withScriptjsProps>;
+    export default function withScriptjs(WrappedComponent: React.Component): React.ComponentClass<withScriptjsProps>;
 }
