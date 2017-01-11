9fab3b481878e955048824e274ad7af03af864ce
Andries Smit <mr.andries.smit@gmail.com>
2017-01-11 17:10:37 +0100

    Fix sourcemapping

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,5 @@
 var webpackConfig = require("./webpack.config");
+const path = require("path");
 Object.assign(webpackConfig, {
     debug: true,
     devtool: "inline-source-map",
@@ -11,6 +12,20 @@ Object.assign(webpackConfig, {
 });
 
 module.exports = function(config) {
+    if(config.instrumenter) {
+        console.log("With instrumenter");
+        Object.assign(webpackConfig, {
+            module: Object.assign(webpackConfig.module, {
+                postLoaders: [ {
+                    test: /\.ts$/,
+                    loader: "istanbul-instrumenter",
+                    include: path.resolve(__dirname, "src"),
+                    exclude: /\.(spec)\.ts$/
+                } ]
+            })
+        });
+    }
+
     config.set({
         basePath: "",
         frameworks: [ "jasmine" ],
@@ -25,7 +40,7 @@ module.exports = function(config) {
         },
         webpack: webpackConfig,
         webpackServer: { noInfo: true },
-        reporters: [ "progress", "kjhtml", "coverage" ],
+        reporters: [ "progress", config.instrumenter ? "coverage": "kjhtml" ],
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
