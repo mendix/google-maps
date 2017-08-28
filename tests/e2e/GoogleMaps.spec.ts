267b08f42128778ff9f78d2ed186b7c6f9f82361
Acellam Guy <abiccel@yahoo.com>
2017-08-28 11:55:12 +0300

    Add e2e tests

--- tests/e2e/GoogleMaps.spec.ts
+++ tests/e2e/GoogleMaps.spec.ts
@@ -1,23 +1,28 @@
-import HomePage from "./pages/home.page";
+import page from "./pages/home.page";
+import { Element } from "webdriverio";
 
 describe("Google maps", () => {
     it("should show single location", () => {
-        HomePage.open();
-    });
+        page.open();
+        page.googleMapNavBarItem.waitForVisible();
+        page.googleMapNavBarItem.click();
+        page.navBarItems.waitForVisible();
+        page.correctAddressBarItem.waitForVisible();
+        page.correctAddressBarItem.click();
+        page.markers.waitForVisible();
 
-    it("should show multiple locations", () => {
-        HomePage.open();
+        const markerList: Element[] = page.markers.value;
+        expect(markerList.length).toBe(1);
+
     });
 
     it("when xpath selected should show locations", () => {
-        HomePage.open();
-    });
+        page.open();
+        page.gridRow1.waitForVisible();
+        page.gridRow1.click();
+        page.markers.waitForVisible();
 
-    it("when latatitude text input changed, marker should change", () => {
-        HomePage.open();
-    });
-
-    it("when address text input changed, marker should change", () => {
-        HomePage.open();
+        const markerList: Element[] = page.markers.value;
+        expect(markerList.length).toBeGreaterThan(1);
     });
 });
--- tests/e2e/pages/home.page.ts
+++ tests/e2e/pages/home.page.ts
@@ -1,4 +1,27 @@
+import { Client, Element, RawResult } from "webdriverio";
+
 class HomePage {
+
+    public get navBarItems(): Client<RawResult<Element[]>> & RawResult<Element[]> {
+        return browser.elements(".mx-navbar-item");
+    }
+
+    public get googleMapNavBarItem(): Client<RawResult<Element>> & RawResult<Element> {
+        return browser.element("#mxui_widget_Navbar_0 > ul > li:nth-child(2)");
+    }
+
+    public get correctAddressBarItem(): Client<RawResult<Element>> & RawResult<Element> {
+        return browser.element("#mxui_widget_Navbar_0 > ul > li.mx-navbar-item.dropdown.open > ul > li:nth-child(1)");
+    }
+
+    public get markers(): Client<RawResult<Element[]>> & RawResult<Element[]> {
+        return browser.elements(".widget-google-maps-marker");
+    }
+
+    public get gridRow1(): Client<RawResult<Element>> & RawResult<Element> {
+        return browser.element(".mx-name-index-0");
+    }
+
     public open(): void {
         browser.url("/");
     }
