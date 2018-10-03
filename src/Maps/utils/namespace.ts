bfe869e74eb76acca344c7fcb7a214ea4a32bc41
SamuelMuloki <samuellebron0@gmail.com>
2018-10-03 16:32:24 +0300

    Update map attributions

--- src/Maps/utils/namespace.ts
+++ src/Maps/utils/namespace.ts
@@ -154,15 +154,15 @@ export namespace MapUtils {
     }
 
     export const customUrls: CustomTypeUrls = {
-        openStreetMap: `http://{s}.tile.osm.org/{z}/{x}/{y}.png`,
+        openStreetMap: `https://{s}.tile.osm.org/{z}/{x}/{y}.png`,
         mapbox: `https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=`,
         hereMaps: `https://2.base.maps.cit.api.here.com/maptile/2.1/maptile/newest/normal.day/{z}/{x}/{y}/256/png8?`
     };
 
     export const mapAttr: MapAttributions = {
-        openStreetMapAttr: `&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors`,
+        openStreetMapAttr: `&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors`,
         mapboxAttr: `Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors,
             <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>`,
-        hereMapsAttr: `Map &copy; 1987-2014 <a href="http://developer.here.com">HERE</a>`
+        hereMapsAttr: `Map &copy; 1987-2014 <a href="https://developer.here.com">HERE</a>`
     };
 }
